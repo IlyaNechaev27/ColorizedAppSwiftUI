17cc507b8e5fc1ad692917162242846cb8864c7b
Ilya Nechaev <dolphins.mind777@gmail.com>
2021-10-30 16:02:40 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Илья Нечаев on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
