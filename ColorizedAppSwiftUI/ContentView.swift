6c5b3d0a52705781eae5a7016a40a06f09d4c58f
Ilya Nechaev <dolphins.mind777@gmail.com>
2021-10-31 19:18:00 +0300

    добавил логику отображения в TF

--- ColorizedAppSwiftUI/ContentView.swift
+++ ColorizedAppSwiftUI/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue: Double = 224
+    @State private var greenSliderValue: Double = 61
+    @State private var blueSliderValue: Double = 94
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            LinearGradient(colors: [.mint, .purple], startPoint: .topLeading, endPoint: .bottomLeading)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(Color(red: redSliderValue / 255, green: greenSliderValue / 255, blue: blueSliderValue / 255))
+                    .overlay(RoundedRectangle(cornerRadius: 15).stroke(.white, lineWidth: 5))
+                    .frame(height: 150)
+                    .padding()
+                
+                Spacer()
+                
+                LabelSliderAndTFBoard(sliderColor: .red, sliderValue: $redSliderValue)
+                LabelSliderAndTFBoard(sliderColor: .green, sliderValue: $greenSliderValue)
+                LabelSliderAndTFBoard(sliderColor: .blue, sliderValue: $blueSliderValue)
+                
+                Spacer()
+            }
+        }
+        .onTapGesture {
+            self.dismissKeyboard()
+        }
+    }
+}
+
+extension View {
+    func dismissKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
@@ -19,3 +53,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
